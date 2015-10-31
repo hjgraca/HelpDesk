4debd1817b5071f3fee9af4ecdf00797c554693d
Henrique Graca <henrique.graca@just-eat.com>
2015-10-31 14:18:13 +0000

    initial commit

---
+++ HelpDesk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelpDesk.Startup))]
+namespace HelpDesk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
